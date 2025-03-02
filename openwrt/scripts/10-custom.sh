dce0824645c397dbf3520173a5e21aa1e24c426e
TuringTrail <107291340+TuringTrail@users.noreply.github.com>
2025-03-02 18:53:38 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -13,8 +13,14 @@ git clone https://github.com/turingtrail/luci-app-syncthing package/new/luci-app
 git clone https://github.com/sbwml/luci-app-xunlei package/new/luci-app-xunlei
 # Lucky大吉
 git clone https://github.com/sirpdboy/luci-app-lucky package/new/luci-app-lucky
+
 # OpenAppFilter
 git clone https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
+# OpenAppFilter旧版本处理
+OFA_PATH="./feeds/packages/net/open-app-filter"
+OFA_LUCI_PATH="./feeds/luci/applications/luci-app-appfilter"
+rm -rf "$OFA_PATH"
+rm -rf "$OFA_LUCI_PATH"
 
 # 更新软件版本
 # 使用 sha256sum 校验HASH
