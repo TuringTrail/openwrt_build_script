94abfbd8d1a7f0d73335d2116d64285fb821565b
TuringTrail <107291340+TuringTrail@users.noreply.github.com>
2025-04-01 23:33:05 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -9,8 +9,10 @@ git clone https://github.com/turingtrail/luci-app-syncthing package/new/luci-app
 git clone https://github.com/sbwml/luci-app-xunlei package/new/luci-app-xunlei
 # Lucky大吉
 git clone https://github.com/sirpdboy/luci-app-lucky package/new/luci-app-lucky
-#EasyTier
+# EasyTier
 git clone https://github.com/EasyTier/luci-app-easytier package/new/luci-app-easytier
+# Nikki (Mihomo)
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/new/luci-app-nikki
 
 # 更新软件版本
 # 使用 sha256sum 校验HASH
